a67c561916bb9420a03892e0099d0daac80baf7a
Kilian Hollmann <kilianhollmann@MacBook-Pro.fritz.box>
2020-09-14 19:01:17 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,62 @@
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<figure>
+	<img alt='Success Kid' src='successkid.jpg'>
+	<figcaption>Have fun with Sapper! Mal gucken</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<form>
+	<div class="form-group">
+	  <label for="exampleInputEmail1">Email address</label>
+	  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+	  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+	</div>
+	<div class="form-group">
+	  <label for="exampleInputPassword1">Password</label>
+	  <input type="password" class="form-control" id="exampleInputPassword1">
+	</div>
+	<div class="form-group form-check">
+	  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+	  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+	</div>
+	<button type="submit" class="btn btn-primary">Submit</button>
+  </form>
